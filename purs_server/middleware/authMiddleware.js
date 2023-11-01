4d7c990ab08b195178363822e0e4c934e91087c8
arav1nd <aravindkayal@constellation4.com>
2023-11-01 18:04:24 -0500

    changes to middleware

--- purs_server/middleware/authMiddleware.js
+++ purs_server/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
         const bearer = header.split(' ');
         const token = bearer[1];
 
-        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {   //verify jwt token
+        jwt.verify(token, "f9b327e70bbcf42494ccb28b2d98e00e", (err, decoded) => {   //verify jwt token
             if (err) {
                 return res.status(403).send({ message: 'Invalid token.' });   // invalid token
             }
